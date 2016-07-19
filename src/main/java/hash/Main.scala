66acc2a18aff741b4eddb6e36f06a7fb0e570c91
HashZhang <zhxhash@vip.sina.com>
2016-07-19 22:38:13 +0800

    Initial commit

---
+++ src/main/java/hash/Main.scala
@@ -0,0 +1,14 @@
+package hash
+
+/**
+  * Created by Hash Zhang on 2016/7/19.
+  */
+object Main {
+  def main(args: Array[String]) {
+    println("************************************************************")
+    println("*********Hello, there! This is for learning scala!**********")
+    println("************************************************************")
+
+    lesson1.test1();
+  }
+}
---
+++ src/main/java/hash/lesson1.scala
@@ -0,0 +1,12 @@
+package hash
+
+/**
+  * Created by Hash Zhang on 2016/7/19.
+  */
+object lesson1 {
+  def test1()={
+    def f1(para1:String,para2:Int)={
+      
+    }
+  }
+}
